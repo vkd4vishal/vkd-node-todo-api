6dc6b58b3d71f485e3c341cc36475de601c09bb7
Vishal Kumar <vishalkd0066@gmail.com>
2019-01-30 14:21:53 +0530

    minor changes

--- server/server.js
+++ server/server.js
@@ -1,15 +1,14 @@
-var {ObjectId} = require('mongodb');
-
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
-var port = process.env.PORT || 3000;
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
+
 
 app.use(bodyParser.json());
 
