0137e561a9062ed050c9e98e0b1a6370749de00b
Vishal Kumar <vishalkd0066@gmail.com>
2019-01-31 20:47:25 +0530

    create database for test and development

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/ToDoApp',{ useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI ,{ useNewUrlParser: true });
 
 //mongodb://<dbuser>:<dbpassword>@ds245523.mlab.com:45523/vkd-todo-api-database
+module.exports ={mongoose};
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -8,7 +10,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ;
 
 app.use(bodyParser.json());
 
@@ -92,6 +94,7 @@ app.patch('/todos/:id', (req, res) => {
       res.status(400).send();
     })
   });
+
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
